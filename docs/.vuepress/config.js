92ae19375bb369c7bbfa9e1e3ce1ae8c1f5265be
Lisianthus-A <lisianthus.zeng@gmail.com>
2021-04-23 19:07:37 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,6 +18,7 @@ module.exports = {
                 // 'css',
                 'react',
                 'typescript',
+                'library',
                 'git',
                 'vuepress',
             ]
